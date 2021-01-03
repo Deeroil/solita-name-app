5809d5458927fbc1db366825c46192a970e20ba3
Miina Saroma <miinasaroma@gmail.com>
2021-01-03 21:20:52 +0200

    Puhdistele koodia, selkeytä muuttujanimiä yms. Melkein toimiva

--- src/App.js
+++ src/App.js
@@ -8,55 +8,47 @@ import nameService from './services/names'
  *    Filtered names aren't shown unless you first press a sort button
  *    getAmount doesn't work
  *    ---> everyone-button renders total amount only if first sorted
- *    ok apua: jos esim filtterinä on vaan V, niin tulos on suoraan amount XDD ei tää oo hyvä...
+ *    todo: jos esim filtterinä on vaan V, niin tulos on suoraan amount XDD ei hyvä...
+ *    todo: kun nappia painetaan, tyhjennetään input sisältö? 
  */
 
 const App = () => {
-  const [names, setNames] = useState([])
+  const [allNames, setAllNames] = useState([])
   const [newFilter, setNewFilter] = useState('')
   //uh oisko joku muu nimi ööhm. nää on filtered or sorted .--.
   //namesList? ööhhmm
-  const [show, setShow] = useState([])
-  // hmmm miten saisin sen Total tohon Names-listailun tilalle sit halutessani??
+  const [shownNames, setShownNames] = useState([])
   
   //haluunko tän kuitenkin vaa laskea aina getAmountilla tjsp :--D
-  const [amount, setAmount] = useState(0)
-
-  const [showNames, setShowNames] = useState(false)
-  const [showAmount, setShowAmount] = useState(false)
+  //totalVisible vai showTotal vaaai
+  //tää on nyt kovin pitkä
+  const [totalAmountVisible, setTotalAmountVisible] = useState(false)
 
   useEffect(() => {
     const getNames = async () => {
       const namesJSON = await nameService.getAll()
-      setNames(namesJSON)
-      console.log('names set')
+      setAllNames(namesJSON)
     }
     getNames()
   }, [])
 
   useEffect(() => {
-    filterList(names)
-    setShowNames(true)
-  }, [newFilter, names])
+    filterList(allNames)
+  }, [newFilter, allNames])
 
-  //vai haluunko että tää vaan suoraa sorttaa _kaikki_, eikä annettua listaa
-  //haluunko että nää return sorted vai suoraan asettaa? Haluunko nimetä uusiksi?
-
-
-  //kauheesti toistoa....
-  const sortByPopularity = (list) => {
-    setShowNames(true)
-    setShowAmount(false)
+ 
+  //kinda duplicatey, but makes the code more readable
+  const sortByPopularity = () => {
+    setTotalAmountVisible(false)
     //slice to make a new array / deep copy
-    const sorted = list.slice().sort((a, b) => b.amount - a.amount)
-    setShow(sorted)
+    const sorted = allNames.slice().sort((a, b) => b.amount - a.amount)
+    setShownNames(sorted)
   }
 
-  const sortAlphabet = (list) => {
-    setShowNames(true)
-    setShowAmount(false)
-    const sorted = list.slice().sort((a, b) => (a.name > b.name) ? 1 : -1)
-    setShow(sorted)
+  const sortAlphabet = () => {
+    setTotalAmountVisible(false)
+    const sorted = allNames.slice().sort((a, b) => (a.name > b.name) ? 1 : -1)
+    setShownNames(sorted)
   }
 
   //jostain syystä kun laitoin totalAmount(list) niin se clogas tän
@@ -70,54 +62,26 @@ const App = () => {
   //no sen voi pilkkoo sit
   //tätä pitää kyl kans hioo, ainakin ton booleanhässäkän kaa.
   const getAmount = (obj) => {
-    //setShowAmount(true)
-    //setShowNames(false)
-    //if obj has amount field, it's probably just one obj
-
     console.log('getAmount obj:', obj)
     console.log('getAmount obj.len:', obj.length)
+
+    //if obj has amount field, it's probably just one obj
     if (obj.amount) {
       return obj.amount
-      //setAmount(obj.amount)
     }
     //otherwise it should be a list of names, return total amount
     if (obj.length > 0) {
       //return obj.reduce((sum, a) => sum + a.amount, 0)
       const total = obj.reduce((sum, a) => sum + a.amount, 0)
-      //setAmount(total)
       console.log('total', total)
       return total
     }
     console.log('RIP getAmount... obj:', obj )
   }
-
-  //voishan sen hakea suoraan apista mutta blää miks ihmeessä
-  // const findAmount = (str) => {
-  //   //joka updatella...
-  //   //jos löytyy: jos löytyy monta: jos löytyy osa
-  //   const found = names.filter(n => n.includes(str))
-
-  //   if (found.length === 1) {
-  //     setFiltered(found.amount) //koska lol tehtävänanto I guess
-  //   } else if (found.length > 1) {
-  //     setFiltered(found)
-  //   } else {
-  //     setFiltered('No-one matched criteria')
-  //   }
-  // }
-
-  //HÄÄH onks siinä järkeä että tää on näin mini :DD
-  const formAmount = (obj) => {
-    return (
-      <div>
-        {getAmount(obj)}
-      </div>
-    )
-  }
  
   const filterList = (list) => {
     //haluunks tarkistaa täällä jo newFilterin pituuden..??
-    setShowAmount(false)
+    setTotalAmountVisible(false)
     const filtered = list
       .filter(o => o.name
         .trim()
@@ -127,17 +91,15 @@ const App = () => {
     console.log('filterList, filtered: ', filtered, typeof filtered)
 
     if (newFilter.length === 0 || filtered.size === 0) {
-      setShow('')
+      setShownNames('')
       console.log('filter and show are empty')
       return
     }
 
-    setShow(filtered)
+    setShownNames(filtered)
 
     if (filtered.length === 1) {
-      setShowAmount(true)
-      setShowNames(false)
-      console.log('Filtered size == 1, showAmount is true, showNames false')
+      setTotalAmountVisible(true)
     }
   }
 
@@ -150,93 +112,36 @@ const App = () => {
     )
   }
 
-  const showConditionally = () => {
-    if ((!showNames && !showAmount) || !show) {
-      return null
-    }
-
-    //filter has only one result
-    if (showAmount && show.length === 1) {
-      console.log('showAmount true, show.len === 1', show)
-      //showNameAmount()
-      console.log('show[0].amount', show[0].amount)
-      setAmount(getAmount(show[0]))
-      setShowNames(false)
-      setShowAmount(true)
-      return (
-        <div>Amount: {amount}</div>
-      )
-    }
-
-    //must be total amount, because more than 1 result in show
-    if (showAmount) {
-      return (
-        <div>
-        <b>Total amount:</b> {amount}
-        </div>
-      )
-    }
-
-    //wait miks tässä on names :DDD
-    if (showNames && names) {
-      console.log('shownames: true, show formNameList:', show)
-      return formNameList(show)
-    } 
-    console.log('RIP conditional rendering...')
-  }
-
-  //aaaaaa nyt se re-renderöi vittuna :DD
-  const showNameAmount = () => {
-    //if (show.length === 1) {
-      setAmount(getAmount(show))
-      setShowNames(false)
-      setShowAmount(true)
-    //}
-    //console.log('oops showNameAmount() didnt work')
-  }
-
-  const showTotalAmount = () => {
-    setAmount(getAmount(names))
-    setShowNames(false)
-    setShowAmount(true)
-  }
-
-  const showStuff = () => {
+  const showResults = () => {
     return (
       <div>
-        {(show.length > 1) ? formNameList(show)
-          : (show.length === 1) ? <div> {getAmount(show)} </div> /*<div> Amount of {show[0].name}s is {show[0].amount}</div>**/
-            : (showAmount) ? <div> total amount is {totalAmount(names)} </div> 
-              : <div>Interact to show data :) show.length is {show.length}</div>}
+        <br/>
+        {(shownNames.length > 1) ? formNameList(shownNames)
+          : (shownNames.length === 1) ? <div> There are {getAmount(shownNames)} {shownNames[0].name}s in Solita </div> /*<div> Amount of {show[0].name}s is {show[0].amount}</div>**/
+            : (totalAmountVisible) ? <div> total amount: {totalAmount(allNames)} </div> 
+              : <div> nothing here :) interact to see data  </div>}
       </div>
     )
   }
 
-  const handleTotalAmount = () => {
-    setShow([])
-    setShowAmount(true)
+  const handleTotalVisibility = () => {
+    setShownNames([])
+    setTotalAmountVisible(true)
   }
 
   return (
     <div>
       <h3>Solita Name App</h3>
       <div>
-        show amount of..
-        <button onClick={() => handleTotalAmount()}>everyone</button>
-        {/* <button onClick={() => showTotalAmount()}>everyone</button> */}
-        {/* --> setShowAmount(true), setShowNames(false),  */}
-        {/* <button onClick={() => totalAmount(names)}>everyone</button> */}
+        <button onClick={handleTotalVisibility}>Total amount</button>
       </div>
       <div>
-        sort by...
-        {/* <Button onClick={() => sortByPopularity(names)} label={'Popularity'} /> */}
-        <button onClick={() => sortByPopularity(names)}>Popularity</button>
-        <button onClick={() => sortAlphabet(names)}>Alphabet</button>
+        Sort by...
+        <button onClick={sortByPopularity}>Popularity</button>
+        <button onClick={sortAlphabet}>Alphabet</button>
       </div>
       <Filter setNewFilter={setNewFilter} />
-      {/* <div>{showConditionally()}</div> */}
-      {showStuff()}
-      {/* ok nää on jotenkin hassusti :DD tietty. Kun tota toista päivitetään jäljessä */}
+      {showResults()}
     </div>
   )
 }
