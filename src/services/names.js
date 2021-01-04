f321fdfb70bd754f558d5c99feef7eb4e41f57cd
Miina Saroma <miinasaroma@gmail.com>
2021-01-04 19:11:38 +0200

    Delete duplicate frontend files

--- src/services/names.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/names'
-
-const getAll = async () => {
-  const response = await axios.get('http://localhost:3003/api/names')
-  return response.data
-}
-
-export default { getAll }
