d248850ccbe76df1dec53cab7b1881f15a88f1ac
Miina Saroma <miinasaroma@gmail.com>
2021-01-05 18:35:21 +0200

    Use static files from UI build

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const cors = require('cors')
 const namesRouter = require('./controllers/names')
 
-//muista cors!
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 app.use(cors())
 app.use(express.json())
 app.use('/api/names', namesRouter)
