88bcdfee9c6839cdf3b8b1a2313854ab3bd3c9c7
Miina Saroma <miinasaroma@gmail.com>
2021-01-07 17:10:03 +0200

    Add route for /:name/amount, edit /:name, fix /total

--- backend/controllers/names.js
+++ backend/controllers/names.js
@@ -5,13 +5,29 @@ namesRouter.get('/', (req, res) => {
   res.json(names)
 })
 
+//up here so it doesn't get mixed up with /:name
+namesRouter.get('/total', (req, res) => {
+  const total = names.map(n => n.amount).reduce((a, sum) => a + sum, 0)
+  res.json(total)
+})
+
 namesRouter.get('/:name', (req, res) => {
   const found = names.find(o => o.name.toLowerCase() === req.params.name.toLowerCase())
 
   if (found) {
-    res.json(found.amount)
+    res.json(found)
   } else {
-    res.send(404).end()
+    res.sendStatus(404).end()
+  }
+})
+
+namesRouter.get('/:name/amount', (req, res) => {
+  const found = names.find(o => o.name.toLowerCase() === req.params.name.toLowerCase())
+
+  if (found) {
+    res.json({ amount: found.amount })
+  } else {
+    res.sendStatus(404).end()
   }
 })
 
@@ -23,11 +39,6 @@ namesRouter.get('/sort/alphabet', (req, res) => {
 namesRouter.get('/sort/popular', (req, res) => {
   const popSorted = names.slice().sort((a, b) => b.amount - a.amount)
   res.json(popSorted)
-})
-
-namesRouter.get('/total', (req, res) => {
-  const total = names.map(n => n.amount).reduce((a, sum) => a + sum, 0)
-  res.json(total)
 })
 
 module.exports = namesRouter
