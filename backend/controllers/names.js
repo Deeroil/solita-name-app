63c36cf8f6f6df4569e55f2717a4f4ea030f3566
Miina Saroma <miinasaroma@gmail.com>
2021-01-07 14:36:08 +0200

    Add api routes for total and pop/alph sorts

--- backend/controllers/names.js
+++ backend/controllers/names.js
@@ -9,10 +9,25 @@ namesRouter.get('/:name', (req, res) => {
   const found = names.find(o => o.name.toLowerCase() === req.params.name.toLowerCase())
 
   if (found) {
-    res.json(found)
+    res.json(found.amount)
   } else {
     res.send(404).end()
   }
+})
+
+namesRouter.get('/sort/alphabet', (req, res) => {
+  const alphSorted = names.map(n => { return { name: n.name } }).sort((a, b) => a.name > b.name ? 1 : -1)
+  res.json(alphSorted)
+})
+
+namesRouter.get('/sort/popular', (req, res) => {
+  const popSorted = names.slice().sort((a, b) => b.amount - a.amount)
+  res.json(popSorted)
+})
+
+namesRouter.get('/total', (req, res) => {
+  const total = names.map(n => n.amount).reduce((a, sum) => a + sum, 0)
+  res.json(total)
 })
 
 module.exports = namesRouter
